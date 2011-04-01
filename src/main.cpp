c03ef65eb0b28b364f1053ff4a82b710d6a964be
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-03-31 23:01:25 -0300

    testing both rotation wise

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,16 @@ int main(int argc, char** argv)
 	base::vec2 v = {{1,2}};
 	
 	base::vec2 c = RotateClockwise(v, 45);
+	base::vec2 d = RotateAntiClockwise(c, 45);
 
 	std::cout << base::unit(v) << std::endl;
 	std::cout << base::unit(c) << std::endl;
+	std::cout << base::unit(d) << std::endl;
 
 	std::cout << base::dot(v,c) << std::endl;
+	std::cout << norm(v)*norm(c)*std::cos(45.f) << std::endl;
+
+	std::cout << base::dot(d,c) << std::endl;
 	std::cout << norm(v)*norm(c)*std::cos(45.f) << std::endl;
 
 	std::cin.get();
