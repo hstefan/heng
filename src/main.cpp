1820c8625cccc0a76110f3c1324827fbc1470a33
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-02 16:32:18 -0300

    testing quadrant function

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 int main(int argc, char** argv)
 {
 	base::vec2 v = {{1,2}};
+	base::vec2 v1 = {{-1,2}};
+	base::vec2 v2 = {{-1,-2}};
+	base::vec2 v3 = {{1, -2}};
+	std::cout << v << "is at the " << (int)base::quadrant(v) << " quadrant" << std::endl;
+	std::cout << v1 << "is at the " << (int)base::quadrant(v1) << " quadrant" << std::endl;
+	std::cout << v1 << "is at the " << (int)base::quadrant(v2) << " quadrant" << std::endl;
+	std::cout << v2 << "is at the " << (int)base::quadrant(v3) << " quadrant" << std::endl;
+	
 	
 	base::vec2 c = RotateClockwise(v, 45);
 	base::vec2 d = RotateAntiClockwise(c, 45);
