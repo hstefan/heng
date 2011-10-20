91abd9520ba6336a2da1b97d2562787d83e779fa
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-10-19 22:37:55 -0200

    Updated project.

---
+++ src/heng/image/ImageLoader.hpp
@@ -0,0 +1,84 @@
+#ifndef HENG_IMAGE_IMAGELOADER_HPP
+#define HENG_IMAGE_IMAGELOADER_HPP
+
+#include <iosfwd>
+#include <stdexcept>
+namespace heng
+{
+namespace image 
+{
+
+class PngException : public std::runtime_error
+{
+public:
+	PngException(const std::string& str);
+};
+
+class Image
+{
+public:
+	Image();
+	Image(unsigned int width, unsigned int height);
+	~Image();
+
+	void initialize(unsigned int width, unsigned int height);
+	void clear();
+
+	unsigned int getWidth() const;
+	unsigned int getHeight() const;
+	unsigned char* getData() const;
+
+	static void loadPNGFileRGBA8(Image& image, std::istream& stream);
+
+private:
+	unsigned int width;
+	unsigned int height;
+	unsigned char* data;
+};
+
+void preMultiplyAlpha(Image& image);
+
+///////////////////////////////////////////////////////////////////////////////
+
+inline PngException::PngException(const std::string& str)
+	: std::runtime_error(str)
+{
+}
+
+inline Image::Image()
+	: width(~0u), height(~0u), data(0)
+{
+}
+
+inline Image::Image(unsigned int width, unsigned int height)
+	: width(~0u), height(~0u), data(0)
+{
+	initialize(width, height);
+}
+
+inline Image::~Image()
+{
+	clear();
+}
+
+inline unsigned int Image::getWidth() const
+{
+	return width;
+}
+
+inline unsigned int Image::getHeight() const
+{
+	return height;
+}
+
+inline unsigned char* Image::getData() const
+{
+	return data;
+}
+
+}
+
+}
+}
+
+#endif
